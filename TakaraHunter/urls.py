1e342f466eb17c6b3c172c1b372ba4cf7b8a9357
Jesus Rodriguez Perez <Jesus@user-PC.(none)>
2013-05-18 01:24:22 +0200

    Arreglado el problema con el static_url. Acuerdate de poner context_instance=RequestContext(request)  en la vista XD

--- TakaraHunter/urls.py
+++ TakaraHunter/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^$','principal.views.inicio'),
+	
+	
+		
+	url('^404testing/$', direct_to_template, {'template': '404.html'}),
+	#url(r'^login/$', 'auth.views.login_user'),
+	url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT,}),
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 )
--- principal/views.py
+++ principal/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.http import HttpResponse, HttpResponseRedirect
 import os
 
 def inicio(request):
-	PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
-	PROJECT_DIR = os.path.join(PROJECT_DIR, 'static'),
+	#PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
+	#PROJECT_DIR = os.path.join(PROJECT_DIR, 'static'),
 	return render_to_response('inicio.html',
 	{
-		'hola':PROJECT_DIR,
-	})
+		'hola':'na',
+	},context_instance=RequestContext(request))
