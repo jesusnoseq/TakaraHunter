d2a4184ee40a728e4530b22761706c19d3cb799c
Jesus Rodriguez Perez <Jesus@user-PC.(none)>
2013-05-30 11:00:24 +0200

    login arreglado

--- principal/forms.py
+++ principal/forms.py
@@ -23,6 +23,7 @@ class PerfilCreationForm(UserCreationForm):
     
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
         user.email = self.cleaned_data["email"]
         if commit:
             user.save()
