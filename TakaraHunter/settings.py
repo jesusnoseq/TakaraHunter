05a86ff0284b6272a07dde30eced7e7495b0ad1e
unknown <Jesus@user-PC.(none)>
2013-12-30 00:25:35 +0100

    pruebas

--- TakaraHunter/settings.py
+++ TakaraHunter/settings.py
@@ -169,19 +169,19 @@ FIXTURE_DIRS = (
 REST_FRAMEWORK = {
     # Use hyperlinked styles by default.
     # Only used if the `serializer_class` attribute is not set on a view.
-    'DEFAULT_MODEL_SERIALIZER_CLASS':
-        'rest_framework.serializers.HyperlinkedModelSerializer',
-        #'rest_framework.renderers.JSONRenderer',
-        #'rest_framework.renderers.BrowsableAPIRenderer',
+    #'DEFAULT_MODEL_SERIALIZER_CLASS':
+    #    'rest_framework.serializers.HyperlinkedModelSerializer',
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    #'DEFAULT_PERMISSION_CLASSES': [
+     #   'rest_framework.permissions.IsAuthenticated',
         #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
+        #'rest_framework.permissions.AllowAny',
+    #],
                   
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
                   
 }
