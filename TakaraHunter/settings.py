e8f5dd745935be888bf1adb0f18fe776a1389020
unknown <Jesus@user-PC.(none)>
2013-12-30 18:57:36 +0100

    JSONPPPPPP

--- TakaraHunter/settings.py
+++ TakaraHunter/settings.py
@@ -180,7 +180,7 @@ REST_FRAMEWORK = {
     #],
                   
     'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.JSONPRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
                   
