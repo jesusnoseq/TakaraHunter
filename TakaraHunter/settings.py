effe776829ee198d50f32348462edffdc446c5af
unknown <Jesus@user-PC.(none)>
2014-01-02 18:20:57 +0100

    rederes

--- TakaraHunter/settings.py
+++ TakaraHunter/settings.py
@@ -187,7 +187,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONPRenderer',
         'rest_framework.renderers.JSONRenderer',
-        #'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
                   
 }
